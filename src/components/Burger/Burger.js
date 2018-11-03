bd1fe22c6974df276f8f8b87861e8d3f3e125e8b
Olga <test@test.com>
2018-11-03 18:01:12 +0200

    add remove ingredients

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import BurgerIgredient from './BurgerIngredient/BurgerIngredient';
+import classes from './Burger.module.css';
+
+const burger = (props) => {
+  let transformedIngredient = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIgredient key={igKey + i} type={igKey}/>
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el)
+    }, []);
+  if (transformedIngredient.length === 0 ) {
+  transformedIngredient = <p>Please start adding ingredients!</p>
+  }
+// console.log(transformedIngredient);
+  return (
+    <div className={classes.Burger}>
+      <BurgerIgredient type='bread-top'/>
+      {transformedIngredient}
+      <BurgerIgredient type='bread-bottom'/>
+    </div>
+  )
+};
+
+export default burger;
+
